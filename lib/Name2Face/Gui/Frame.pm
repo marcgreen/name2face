435d736e328e6938879b667c905b0d692358b769
Marc Green <marcgreen@wpi.edu>
2011-12-27 22:02:10 -0500

    More work on sizers

--- lib/Name2Face/Gui/Frame.pm
+++ lib/Name2Face/Gui/Frame.pm
@@ -5,7 +5,7 @@ use base "Wx::Frame";
 use warnings;
 use 5.14.0;
 use Data::Dumper;
-use Wx qw/:id :sizer/;
+use Wx qw/:everything/;
 use Wx::Event qw/EVT_BUTTON/;
 
 sub new {
@@ -14,73 +14,78 @@ sub new {
         undef,           # parent window
         -1,              # ID -1 means any
         'Name2Face',     # title
+        wxDefaultPosition,
+        [700,400],
         );
-
-    $self->{'panel'} = Wx::Panel->new($self);
 
     my $outerV = Wx::BoxSizer->new(wxVERTICAL);
     my $introH = Wx::BoxSizer->new(wxHORIZONTAL);
-    my $left_introV = Wx::BoxSizer->new(wxVERTICAL);
-    my $right_introV = Wx::BoxSizer->new(wxVERTICAL);
     my $headingH = Wx::BoxSizer->new(wxHORIZONTAL);
 
-   $left_introV->Add(
-       Wx::StaticText->new(
-           $self->{'panel'},     # Parent window
-           -1,         # no window ID
-           'Add as many sections as you want, they will appear in fields below:',
-       ),
-       0, 0, wxALL, 5);
-
+    # left side of the instructions
+    my $l_instr_panel = Wx::Panel->new($self, -1, [-1, -1], [300, 100]);
+    my $instr1 = Wx::StaticText->new(
+        $l_instr_panel,     # Parent window
+        -1,         # no window ID
+        'Add as many sections as you want, they will appear in fields below:',
+        );
+    $instr1->Wrap(300);
     my $addSectionBtn = Wx::Button->new(
-        $self->{'panel'},
+        $l_instr_panel,
         -1,
-        'Add a Section',);
+        'Add a Section',
+        [0, 40]
+        );
 
-    $left_introV->Add($addSectionBtn, 0, 0, wxALL, 5);
-
-   $right_introV->Add(
-       Wx::StaticText->new(
-           $self->{'panel'},
-           -1,
-           'When you are done adding Sections, generate the PDF files:',),
-       0, 0, wxALL, 5);
-
+    # right side of the instructions
+    my $r_instr_panel = Wx::Panel->new($self, -1, [-1, -1], [300, 100]);
+    my $instr2 = Wx::StaticText->new(
+        $r_instr_panel,
+        -1,
+        'When you are done adding sections, generate the PDF files:',
+        );
+    $instr2->Wrap(300);
     my $genPDFBtn = Wx::Button->new(
-        $self->{'panel'},
+        $r_instr_panel,
         -1,
-        'Generate PDFs',);
+        'Generate PDFs',
+        [0, 40]
+        );
 
-    $right_introV->Add($genPDFBtn, 0, 0, wxALL, 5);
+    $introH->Add($l_instr_panel, 0, wxALL, 20);
+    $introH->AddStretchSpacer(1);
+    $introH->Add($r_instr_panel, 0, wxALL, 20);
 
-    $introH->Add($left_introV);
-    $introH->Add($right_introV);
+    # XXX why do the spacers overlap?
 
-    $headingH->Add(
-        Wx::StaticText->new(
-            $self->{'panel'},
-            -1,
-            'Path to Section',),);
+    # heading for sections
+    my $l_head_panel = Wx::Panel->new($self, -1, [-1, -1], [100, 20]);
+    my $head1 = Wx::StaticText->new(
+        $l_head_panel,
+        -1,
+        'Path to Section',);
+    my $r_head_panel = Wx::Panel->new($self, -1, [-1, -1], [100, 20]);
+    my $head2 = Wx::StaticText->new(
+        $r_head_panel,
+        -1,
+        'Name of generated file(s)',);
 
-    $headingH->Add(
-        Wx::StaticText->new(
-            $self->{'panel'},
-            -1,
-            'Name of generated file(s)',),);
+    $headingH->Add($l_head_panel, 0, wxALL, 20);
+    $headingH->AddStretchSpacer(1);
+    $headingH->Add($r_head_panel, 0, wxALL, 20);
+
 
     $self->{'sectionYInc'} = 35; # how much to increment the Y value each time
     $self->{'sectionY'} = 110-$self->{'sectionYInc'};
       # at what Y value the section lines will start
 
     $outerV->Add($introH);
-    $outerV->Add($headingH);
+#    $outerV->Add($headingH);
 
     EVT_BUTTON($self, $addSectionBtn, \&OnAddSection);
-    EVT_BUTTON($self, $genPDFBtn, \&OnGenPDF);
+#    EVT_BUTTON($self, $genPDFBtn, \&OnGenPDF);
 
-    $self->SetAutoLayout(1);
     $self->SetSizer($outerV);
-    $outerV->SetSizeHints($self);
     return $self;
 }
 
@@ -90,7 +95,7 @@ sub addSectionLine {
     $self->{'sectionY'} += $self->{'sectionYInc'};
 
     # path
-    my $p = Wx::TextCtrl->new($self->{'panel'},
+    my $p = Wx::TextCtrl->new($self,
                       -1,
                       $path,
                       [20,$self->{'sectionY'}],
@@ -98,7 +103,7 @@ sub addSectionLine {
         );
 
     # name
-    my $n = Wx::TextCtrl->new($self->{'panel'},
+    my $n = Wx::TextCtrl->new($self,
                       -1,
                       "$path.pdf",
                       [380,$self->{'sectionY'}],
@@ -107,7 +112,7 @@ sub addSectionLine {
 
     # delete
     # XXX add icon bmp
-    my $del = Wx::Button->new($self->{'panel'},
+    my $del = Wx::Button->new($self,
                               -1,
                               'Delete',
                               [640,$self->{'sectionY'}],
