934f9974755a2f9fb7586718bf8698757a668fe0
Marc Green <marcgreen@wpi.edu>
2012-03-06 16:45:36 -0500

    Remove "--out" option as it is confusing and not that useful

--- name2face.pl
+++ name2face.pl
@@ -14,20 +14,20 @@ $ENV{'PATH'} .= ':/home/marcgreen/htmldoc/bin' if $ENV{'PATH'} !~ /htmldoc/;
 GetOptions(
     'html!'  => \(my $Gen_html = 0),# Don't generate .html by default
     'pdf!'   => \(my $Gen_pdf = 1), # Generate .pdf by default
-    'out=s@' => \(my $Outfile),     # Name of generated pdf/html
-    #   Default name is directory (arg) name
 );
 
 my @sections = @ARGV;
 usage() unless @sections;
 
 my %sects;
-while (my ($index, $dir) = each @sections) { # index used with $Outfile
-    $dir = File::Spec->catdir($dir); # remove trailing / for naming purposes
-    $sects{$dir} = ($Outfile->[$index] // $dir);
+for (@sections) {
+    my $dir = File::Spec->catdir($_); # remove trailing / for naming purposes
+    $sects{$dir} = $dir;
 }
 
 my $n2f = Name2Face::Base->new(output_html => $Gen_html, output_pdf => $Gen_pdf);
+# %sects is given as a hash to allow the user to supply a different filename
+# than the name of the directory. This feature has been removed for now
 $n2f->name2face(%sects);
 
 sub usage {
@@ -37,16 +37,11 @@ sub usage {
     a table of student's names and their pictures, among other small details.
     The purpose is to help professors put names to their student's faces.
 
-Usage: perl $0 [options] -- [sections to process]
-E.g., perl $0 --html --out classlist1 --out "class list 2"
-                        -- Section1/ Section2/
+Usage: /home/marcgreen/n2f/name2face [options] -- [sections to process]
+E.g.,  /home/marcgreen/n2f/name2face --html Section1/ Section2/
 
 Options
   --[no]html   generate an html file of the finished output (off by default).
-  --out <name> extensionless filename given to the generated files (default is
-               the name of the directory being processed, i.e., the argument
-               given to the program). You can pass multiple --out=<name> options
-               and the nth one will be applied to the nth section.
   --[no]pdf    generate a pdf file of the finished output (on by default).
 
     The "section" to be processed ought to be a directory of a downloaded
